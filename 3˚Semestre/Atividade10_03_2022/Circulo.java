26259f69e4c2aaf35573b72049a58d3fd87ae2df
Diego Daniel <76542858+diegodanielsantana00@users.noreply.github.com>
2022-03-12 23:16:17 -0300

    12/03

---
+++ 3˚Semestre/Atividade10_03_2022/Circulo.java
@@ -0,0 +1,64 @@
+// import java.util.Scanner;
+
+// public class Circulo {
+//     private double raio;
+
+//     public Circulo() {
+//         this.raio = 2;
+//         CalcularCircuferencia(this);
+//     }
+
+//     public double RetornoRaio() {
+//         return this.raio;
+//     }
+
+//     public void CadastrarRaio() {
+//         Scanner in = new Scanner(System.in);
+
+//         System.out.print("Informe o novo Raio: ");
+//         this.raio = in.nextDouble();
+
+//         System.out.println("Nova Circunferência: " + CalcularCircuferencia(this) + " ");
+//     }
+
+//     public void VerificarPontos() {
+//         double pontos[];
+//         pontos = new double[5];
+//         String retorno = "";
+//         Scanner in = new Scanner(System.in);
+
+//         System.out.print("Informe o 1 Ponto: ");
+//          pontos[0] = in.nextDouble();
+//         System.out.print("Informe o 2 Ponto: ");
+//          pontos[1] = in.nextDouble();
+//         System.out.print("Informe o 3 Ponto: ");
+//          pontos[2] = in.nextDouble();
+//         System.out.print("Informe o 4 Ponto: ");
+//          pontos[3] = in.nextDouble();
+//         System.out.print("Informe o 5 Ponto: ");
+//          pontos[4] = in.nextDouble();
+
+//          for (int i = 0; i < pontos.length; i++) {
+//             double circuferenciaTemp = 2 * Math.PI * pontos[i];
+//              if(circuferenciaTemp <= CalcularCircuferencia(this)){
+//                 retorno += pontos[i] + ", ";
+//              }
+//          }
+
+//         System.out.println("Os pontos " + retorno + " Estão dentro da circuferencia ");
+//     }
+
+//     // public String ExibirEquacao() {
+//     // return this.x2 + "x² + " + this.x + "x + " + this.c + " = 0";
+//     // }
+
+//     public double CalcularCircuferencia(Circulo circulo) {
+
+//         // #region Calculo Circuferencia
+//         double circuferencia = 2 * Math.PI * circulo.raio;
+//         // #endregion
+
+//         return circuferencia;
+//     }
+
+// }
